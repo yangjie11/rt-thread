ccf68bf9d19944b64b184c38d46831bbc292f6b9
yangjie11 <39754270+yangjie11@users.noreply.github.com>
2021-01-05 11:01:33 +0800

    Update rtdef.h

--- include/rtdef.h
+++ include/rtdef.h
@@ -55,7 +55,7 @@ extern "C" {
 /* RT-Thread version information */
 #define RT_VERSION                      4L              /**< major version number */
 #define RT_SUBVERSION                   0L              /**< minor version number */
-#define RT_REVISION                     3L              /**< revise version number */
+#define RT_REVISION                     4L              /**< revise version number */
 
 /* RT-Thread version */
 #define RTTHREAD_VERSION                ((RT_VERSION * 10000) + \
